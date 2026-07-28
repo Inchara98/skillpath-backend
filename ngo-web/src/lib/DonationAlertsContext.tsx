8245fbaad9a20f35ba5139d1bdc071c9a2481573
Inchara98 <inchara.gowda@tibilsolutions.com>
2026-07-28 09:54:21 +0000

    Reduce polling interval from 15s to 5s for faster updates

--- ngo-web/src/lib/DonationAlertsContext.tsx
+++ ngo-web/src/lib/DonationAlertsContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useRef, useState, type ReactNode } from 'react'
 import { fetchDonationRequests, type DonationRequest } from './ngoRequestsApi'
 
-const POLL_INTERVAL_MS = 15000
+const POLL_INTERVAL_MS = 5000
 const TOAST_DURATION_MS = 6000
 
 interface DonationAlertsValue {
